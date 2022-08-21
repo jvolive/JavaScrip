bee2d2b26a1ebd4fd9d031d5a8e4454536b0124a
joaoo <joaooliveira1999@outlook.com>
2022-08-21 01:56:01 -0300

    aula2

---
+++ aula2.js
@@ -0,0 +1,85 @@
+// //video 1
+// // var
+
+// // var altura = 5;
+// // var comprimento=7;
+
+// // area=altura*comprimento;
+// // console.log(area)
+// // var area;
+
+// // // let
+// // let forma = "retangulo";
+// // let altura = 5;
+// // let comprimento = 7;
+// // let area;
+
+// // if (forma === "retangulo") {
+// //     area = altura * comprimento;
+// // } else{
+// //     area = altura * comprimento / 2;
+// // }
+
+// // console.log(area)
+
+// // const
+
+// const forma = "triangulo";
+// const altura = 5;
+// const comprimento = 7;
+// let area;
+
+// if (forma === "quadrado") {
+//   area = altura * comprimento;
+// } else {
+//   area = (altura * comprimento) / 2;
+// }
+
+// console.log("video 1", area);
+
+// // video 2
+// //boolean
+// const usuarioLogado = true;
+// const contaPaga = false;
+
+// //truthy or falsy
+// // 0=> false
+// //1=> true
+
+// console.log(0 == false);
+// console.log("" == false);
+// console.log(1 == true);
+
+// //undefined
+// //null
+
+// let minhaVar;
+// let varNull = null;
+
+// console.log(minhaVar);
+// console.log(varNull);
+
+// let numero = 3;
+// let texto = "joao";
+
+// console.log(typeof numero);
+// console.log(typeof texto);
+// console.log(typeof minhaVar);
+// console.log(typeof varNull);
+
+// video 3
+
+// tipo de dados
+// booleanos
+
+// conversao implicita
+const numero1 = 456;
+const numeroString = "456";
+
+//console.log(numero1===numeroString) //false
+// console.log(numero1 == numeroString); // true
+
+// conversao da string em number com number ()
+console.log(numero1 + Number(numeroString));
+
+// conversao explicita
